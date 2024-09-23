badb9437f917529d610789659fb6301e8a9ff290
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2024-09-23 14:27:27 +0530

    Few Changes in Video

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const collectionProductRoutes = require('./routes/collectionProductRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 const videoRoutes = require('./routes/videoRoutes');
 const orderRoutes = require('./routes/orders');
-
+const mediaRoutes = require('./routes/mediaRoutes');
 
 // Load environment variables
 dotenv.config();
@@ -37,6 +37,7 @@ app.use('/api/collection-products', collectionProductRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api', videoRoutes);
 app.use('/api', orderRoutes);
+// app.use('/api/media', mediaRoutes);
 
 // Serve static files from the front-end build folder if you have a front-end
 app.use(express.static(path.join(__dirname, 'build')));
